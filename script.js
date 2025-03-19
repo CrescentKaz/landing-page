d30c1a135ad06dd3d197dbed3c5b8c6cd9f5ff5c
CrescentKaz <121899324+CrescentKaz@users.noreply.github.com>
2025-03-19 12:06:44 -0500

    need to initialize buttons before they can be accessed

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+home_btn.onclick = goHome;
+project_btn.onclick = goProject;
+about_btn.onclick = goAbout;
+social_btn.onclick = goSocial;
+
 const title_section = document.getElementById(title_section);
 const project_section = document.getElementById(project_section);
 const about_me = document.getElementById(about_me);
@@ -7,11 +12,6 @@ const home_btn = document.getElementById(home_btn);
 const project_btn = document.getElementById(project_btn);
 const about_btn = document.getElementById(about_btn);
 const social_btn = document.getElementById(social_btn);
-
-home_btn.onclick = goHome;
-project_btn.onclick = goProject;
-about_btn.onclick = goAbout;
-social_btn.onclick = goSocial;
 
 function goHome() {
     title_section.style.display = "contens";
