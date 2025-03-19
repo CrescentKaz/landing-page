2210c224a13e08d52b870a0d345afc7da4fdff8e
CrescentKaz <121899324+CrescentKaz@users.noreply.github.com>
2025-03-19 12:25:07 -0500

    updated IDs, troubleshot JS, changed color roots and the background.

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
-home_btn.onclick = goHome;
-project_btn.onclick = goProject;
-about_btn.onclick = goAbout;
-social_btn.onclick = goSocial;
+const titleSection = document.getElementById(title_section);
+const projectSection = document.getElementById(project_section);
+const aboutSection = document.getElementById(about_section);
+const socialSection = document.getElementById(social_section);
 
-const title_section = document.getElementById(title_section);
-const project_section = document.getElementById(project_section);
-const about_me = document.getElementById(about_me);
-const socials = document.getElementById(socials);
+const homeBtn = document.getElementById(home_btn);
+const projectBtn = document.getElementById(project_btn);
+const aboutBtn = document.getElementById(about_btn);
+const socialBtn = document.getElementById(social_btn);
 
-const home_btn = document.getElementById(home_btn);
-const project_btn = document.getElementById(project_btn);
-const about_btn = document.getElementById(about_btn);
-const social_btn = document.getElementById(social_btn);
+homeBtn.onclick = goHome;
+projectBtn.onclick = goProject;
+aboutBtn.onclick = goAbout;
+socialBtn.onclick = goSocial;
 
 function goHome() {
-    title_section.style.display = "contens";
-    project_section.style.display = "none";
-    about_me.style.display = "none";
-    socials.style.display = "none";
+    titleSection.style.display = "contens";
+    projectSection.style.display = "none";
+    aboutSection.style.display = "none";
+    socialSection.style.display = "none";
 };
 
 function goProject() {
-    title_section.style.display = "none";
-    project_section.style.display = "contents";
-    about_me.style.display = "none";
-    socials.style.display = "none";
+    titleSection.style.display = "none";
+    projectSection.style.display = "contents";
+    aboutSection.style.display = "none";
+    socialSection.style.display = "none";
 };
 
 function goAbout() {
-    title_section.style.display = "none";
-    project_section.style.display = "none";
-    about_me.style.display = "contents";
-    socials.style.display = "none";
+    titleSection.style.display = "none";
+    projectSection.style.display = "none";
+    aboutSection.style.display = "contents";
+    socialSection.style.display = "none";
 };
 
 function goSocial() {
-    title_section.style.display = "none";
-    project_section.style.display = "none";
-    about_me.style.display = "none";
-    socials.style.display = "contents";
+    titleSection.style.display = "none";
+    projectSection.style.display = "none";
+    aboutSection.style.display = "none";
+    socialSection.style.display = "contents";
 };
